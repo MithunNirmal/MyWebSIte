d3122d67c263c19d9fb6ecd3996935de7b3a0f2b
Mithun Nirmal <srmithunnirmal@gmail.com>
2024-10-29 21:16:17 +0530

    product and cart related code removal

--- src/main/java/com/mithunnirmal/merch/repositories/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mithunnirmal.merch.repositories;
-
-import com.mithunnirmal.merch.entities.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends JpaRepository<Product, String> {
-}
